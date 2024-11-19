7c9f75675048c4e1565675e149b7612f4d6144de
Laura Elizabeth Moralez <112983807+emoralez19@users.noreply.github.com>
2024-11-19 11:10:17 -0600

    Add files via upload

---
+++ JigsawScript.cs
@@ -0,0 +1,133 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class JigsawScript : MonoBehaviour
+{
+    [SerializeField] private Transform emptySpace = null;
+    private Camera camera;
+    [SerializeField] public TileScript[] tiles;
+    [SerializeField] public TileScript[] tile;
+    private int emptySpaceIndex = 9;
+    private bool isFinished;
+    [SerializeField] private GameObject endPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+       // Shuffle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            if (hit)
+            {
+                if (Vector2.Distance(emptySpace.position, hit.transform.position)< 5)
+                {
+                    Vector2 lastEmptySpacePosition = emptySpace.position;
+                    TileScript thisTile = hit.transform.GetComponent<TileScript>();
+                    emptySpace.position = thisTile.TargetPosition;
+                    thisTile.TargetPosition = lastEmptySpacePosition;
+                    int tileIndex = findIndex(thisTile); 
+                    tiles[emptySpaceIndex] = tiles[tileIndex];
+                    tiles[tileIndex] = null;
+                    emptySpaceIndex = tileIndex;
+                }
+            }
+        }
+        if (!isFinished)
+        { 
+            int correctTiles = 0;
+            foreach (var a in tiles)
+            {
+                if (a != null)
+                {
+                    if (a.inRightPlace)
+                    {
+                        correctTiles++;
+                        Debug.Log("a in right place");
+                    }
+                    // Win State
+                    if (correctTiles == tiles.Length - 1)
+                    {
+                        Debug.Log("In the win state");
+                        isFinished = true;
+                        endPanel.SetActive(true);
+                    }
+                }
+            } 
+        }
+    }
+
+    public void Shuffle()
+    {
+        if (emptySpaceIndex != 9)
+        {
+            var tileOn9LastPos = tiles[9].TargetPosition;
+            tiles[9].TargetPosition = emptySpace.position;
+            emptySpace.position = tileOn9LastPos;
+            tiles[emptySpaceIndex] = tiles[9];
+            tiles[9] = null;
+            emptySpaceIndex = 9;
+        }
+        int invertion;
+        do
+        {
+            for (int i = 0; i <= 7; i++)
+            {
+                var lastPos = tiles[i].TargetPosition;
+                int randomIndex = UnityEngine.Random.Range(0, 7);
+                tiles[i].TargetPosition = tiles[randomIndex].TargetPosition;
+                tiles[randomIndex].TargetPosition = lastPos;
+                var tile = tiles[i];
+                tiles[i] = tiles[randomIndex];
+                tiles[randomIndex] = tile;
+
+            }
+            invertion = GetInversions();
+        } while (invertion % 2 != 0);
+       
+    }
+
+    public int findIndex(TileScript ts)
+    {
+        for (int i =0; i < tiles.Length; i++) 
+        {
+            if (tiles[i] != null)
+            {
+                if (tiles[i] == ts)
+                {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    int GetInversions()
+    {
+        int inversionsSum = 0;
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            int thisTileInversion = 0;
+            for (int j = i; j < tiles.Length; j++)
+            {
+                if (tiles[j] != null)
+                {
+                    if (tiles[i].number > tiles[j].number)
+                    {
+                        thisTileInversion++;
+                    }
+                }
+            }
+            inversionsSum += thisTileInversion;
+        }
+        return inversionsSum;
+    }
+}
